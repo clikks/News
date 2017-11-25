088cf9c6b9395a7b1e06215407def97d6b898826
popcorn <anycosmo@qq.com>
2017-11-25 17:05:55 +0800

    challenge 6 finish

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import os, json
 from collections import OrderedDict
 
@@ -10,7 +10,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 class Analyze_json:
 	def __init__(self,path):
-		self._file_list = list()
+		self._file_dict = dict()
 		self._path = path
 
 	def _confirm_json_file(self):
@@ -18,17 +18,18 @@ class Analyze_json:
 			file_path = os.path.join(self._path,file)	
 			if os.path.isfile(file_path):
 				if 'json' in file:
-					self._file_list.append(file_path) 
+					self._file_dict[file_path] = file.split('.')[0]
+
 	@property
 	def json_dict(self):
 		self._confirm_json_file()
 		self.info_dict = OrderedDict()
-		for json_file in self._file_list:
+		for json_file,file_name in self._file_dict.items():
 			with open(json_file, 'r') as file:
 				json_file_dict = json.loads(file.read())
 				for key,value in json_file_dict.items():
 					if key == 'title':
-						self.info_dict[value] = json_file_dict
+						self.info_dict[file_name] = json_file_dict
 		return self.info_dict
 
 
@@ -37,9 +38,24 @@ ALL_JSON_INFO = Analyze_json('/home/shiyanlou/files').json_dict
 @app.route('/')
 def index():
 	TITLE_LIST =list()
-	for key in ALL_JSON_INFO.keys():
-		TITLE_LIST.append(key)
+	for value in ALL_JSON_INFO.values():
+		title = value.get('title')
+		TITLE_LIST.append(title)
 	return render_template('index.html', title_list = TITLE_LIST)
 
+@app.route('/files/<filename>')
+def file(filename):
+	# article = dict()
+	if filename in ALL_JSON_INFO.keys():
+		# value = ALL_JSON_INFO.get(filename)
+		article = ALL_JSON_INFO.get(filename)
+		return render_template('file.html',article=article)
+	else:
+		return render_template('404.html')
 
-
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'),404
+# @app.errorhandler(404)
+# def not_found(error):
+# 	return render_template('404.html'),404
