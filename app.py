49ce8d6fa6c50a4c7eb8ea0f78864d34b52ba0a8
popcorn <anycosmo@qq.com>
2017-11-27 22:49:58 +0800

    update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 
 class File(db.Model):
 	__tablename__ = 'file'
-	__table_args__ = ''
+	__table_args__ = {"useexisting":True}
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(80))
 	create_time = db.Column(db.DateTime)
@@ -24,12 +24,12 @@ class File(db.Model):
 	content = db.Column(db.Text)
 	category = db.relationship('Category',backref='files')
 
-	def __init__(self,title,category_id,content,create_time=None):
+	def __init__(self,title,create_time,category,content,):
 		self.title = title
-		self.category_id = category_id
+		self.category = category
 		self.content = content
-		if create_time is None:
-			create_time = datetime.utcnow()
+		# if create_time is None:
+		# 	create_time = datetime.utcnow()
 		self.create_time = create_time
 
 	def __repr__(self):
@@ -37,6 +37,7 @@ class File(db.Model):
 
 class Category(db.Model):
 	__tablename__ = 'category'
+	__table_args__ = {"useexisting":True}
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 
@@ -73,27 +74,33 @@ class Category(db.Model):
 
 # ALL_JSON_INFO = Analyze_json('/home/shiyanlou/files').json_dict
 
-# @app.route('/')
-# def index():
-# 	TITLE_LIST =list()
-# 	for value in ALL_JSON_INFO.values():
-# 		title = value.get('title')
-# 		TITLE_LIST.append(title)
-# 	return render_template('index.html', title_list = TITLE_LIST)
 
-# @app.route('/files/<filename>')
-# def file(filename):
-# 	# article = dict()
-# 	if filename in ALL_JSON_INFO.keys():
-# 		# value = ALL_JSON_INFO.get(filename)
-# 		article = ALL_JSON_INFO.get(filename)
-# 		return render_template('file.html',article=article)
-# 	else:
-# 		return render_template('404.html')
+@app.route('/')
+def index():
+	# TITLE_LIST =list()
+	# for value in ALL_JSON_INFO.values():
+	# 	title = value.get('title')
+	# 	TITLE_LIST.append(title)
+	TITLE_LIST = db.session.query(File.title,File.id).all()
+	return render_template('index.html', title_list = TITLE_LIST)
 
-# @app.errorhandler(404)
-# def not_found(error):
-# 	return render_template('404.html'),404
-# @app.errorhandler(404)
-# def not_found(error):
-# 	return render_template('404.html'),404
+@app.route('/files/<file_id>')
+def file(file_id):
+	# article = dict()
+	All_rticle_id = db.session.query(File.id).all()
+	article_id_list = [i[0] for i in All_article_id]
+	
+	if fileid in article_id_list:
+		article = File.query.filter(File.id==file_id)
+		# value = ALL_JSON_INFO.get(filename)
+		# article = ALL_JSON_INFO.get(filename)
+		return render_template('file.html',article=article)
+	else:
+		return render_template('404.html')
+
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'),404
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'),404
